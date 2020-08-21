3016c5058e07a776cc2f15343cd608b7a0bb0585
ishdevops <isubedar84@gmail.com>
2020-08-21 23:02:59 +0100

    changes to NAT and IGW

--- vpc.tf
+++ vpc.tf
@@ -146,7 +146,7 @@ resource "aws_route_table" "nat-routetable" {
 resource "aws_route" "nat-route" {
     route_table_id = aws_route_table.nat-routetable.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat.id
     depends_on = [
         aws_route_table.nat-routetable
     ]
