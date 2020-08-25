11da1674c5bf2ec5522d3da3fa2c4e7a4575d980
ishdevops <isubedar84@gmail.com>
2020-08-25 12:36:34 +0100

    changes to NAT route

--- vpc.tf
+++ vpc.tf
@@ -129,13 +129,12 @@ resource "aws_eip" "elastic_ip" {
 #route for Nat-Gateway
 resource "aws_route" "nat_route" {
     route_table_id = aws_route_table.public_route_table.id
-    destination_cidr_block = "0.0.0.0/0"
+    //destination_cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
     depends_on = [
         aws_route_table.public_route_table
     ]
 }
-
 
 
 
