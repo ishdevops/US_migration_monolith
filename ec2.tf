d7abaf836b100e9f7fd6c4963e58ccda89d17db7
ishdevops <isubedar84@gmail.com>
2020-10-13 12:30:01 +0100

    changes to subnet ids

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "prod_sql_server" {
     ami                         = "ami-04540020f28b7463c"
     instance_type               = "r5a.2xlarge"
-    subnet_id                   = "private_subnet_a"
+    subnet_id                   = aws_subnet.private_subnet_a.id
     vpc_security_group_ids      = ["sql_server_sg", "windows_rdp"]
     associate_public_ip_address = "false"
     disable_api_termination     = "true"
@@ -14,7 +14,7 @@ resource "aws_instance" "prod_sql_server" {
 resource "aws_instance" "jumpbox" {
     ami                         = "ami-0b9482d3216554966"
     instance_type               = "t3a.medium"
-    subnet_id                   = "public_subnet_a"
+    subnet_id                   = aws_subnet.public_subnet_a.id
     vpc_security_group_ids      = ["jumpbox_sg", "sql_server_sg", "windows_rdp"]
     associate_public_ip_address = "true"
     disable_api_termination     = "true"
