6b979b384e48e6532d64c43c34394cd88592a031
ishdevops <isubedar84@gmail.com>
2020-10-15 12:33:36 +0100

    changes to route53.tf

--- route53.tf
+++ route53.tf
@@ -4,16 +4,16 @@ resource "aws_route53_zone" "salaryfinance_hosted_zone" {
 
 resource "aws_route53_record" "us-battasks" {
   zone_id = aws_route53_zone.salaryfinance_hosted_zone.zone_id
-  name    = "us-battasks.salaryfinance.club"
-  type    = "A"
-  ttl     = "300"
+  name    = "us-battasks"
+  type    = "CNAME"
+  ttl     = "60"
   records = [aws_lb.restricted_access_lb.id]
 }
 
 resource "aws_route53_record" "us-battasksapi" {
   zone_id = aws_route53_zone.salaryfinance_hosted_zone.zone_id
-  name    = "us-battasksapi.salaryfinance.club"
-  type    = "A"
-  ttl     = "300"
+  name    = "us-battasksapi"
+  type    = "CNAME"
+  ttl     = "60"
   records = [aws_lb.restricted_access_lb.id]
 }
